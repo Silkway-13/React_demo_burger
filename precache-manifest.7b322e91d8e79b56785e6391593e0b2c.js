f9dc3e0b5127a77463e221ffc05c791e8de25d31
bayr.khan <bayrsaikhan.ger13@gmail.com>
2023-08-09 13:08:38 +0800

    Updates

---
+++ precache-manifest.7b322e91d8e79b56785e6391593e0b2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f83ed303acbceeaf9a173ecb6a91271",
+    "url": "/React_demo_burger/index.html"
+  },
+  {
+    "revision": "f186bf58d69e314d0613",
+    "url": "/React_demo_burger/static/css/main.87de219f.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/React_demo_burger/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React_demo_burger/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f186bf58d69e314d0613",
+    "url": "/React_demo_burger/static/js/main.3f40abbd.chunk.js"
+  },
+  {
+    "revision": "60b319ef8eb8481303ec",
+    "url": "/React_demo_burger/static/js/runtime-main.0ef85f01.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React_demo_burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
